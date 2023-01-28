908f643d6b1f6799ed09b265a1c30cf75c5f152b
Katarzyna Debowska <katarzyna.debowska@host.com>
2023-01-28 13:57:37 +0100

    changing redirects in hot seat

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ from flask import render_template
 app = Flask(__name__)
 
 from inc.game import Game
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 #  SOLO ########################################################################################
 
@@ -16,9 +19,10 @@ def home():
 def solo_form():
     if request.method == 'POST':
         players = [x for x in request.form.getlist('names') if x]
-        if len(players)==4:
+        if len(players) == 4:
             return solo_new_game(players)
     return render_template('solo/form.html')
+
 
 def solo_new_game(name):
     global game
@@ -28,7 +32,7 @@ def solo_new_game(name):
 
 @app.route('/solo/phase1')
 def solo_phase1():
-    if (len([player for player in game.players if player.count_player_cards() > 0])==0 and 7 >= game.round) or game.round==0:
+    if len([player for player in game.players if player.count_player_cards() > 0]) == 0 and game.round <= 7:
         return redirect('/solo/end_of_round', code=302)
     if game.table.count_cards() == 4:
         game.table.cards = []
@@ -37,7 +41,8 @@ def solo_phase1():
             for card in game.players[game.active_player].hand:
                 if card.color == game.table.cards[0].color:
                     if game.round == 7:
-                        index = game.players[game.active_player].tactic_have_color[game.round](game.table.cards, card, game.played_cards)
+                        index = game.players[game.active_player].tactic_have_color[game.round](game.table.cards, card,
+                                                                                               game.played_cards)
                         game.play(index)
                         return redirect('/solo/next_player', code=302)
                     index = game.players[game.active_player].tactic_have_color[game.round](game.table.cards, card)
@@ -54,25 +59,27 @@ def solo_phase1():
     else:
         return render_template('solo/throw_card.html', game=game)
 
+
 @app.route('/solo/throw_card/<int:index>')
 def solo_throw_card(index):
-    if game.table.count_cards()!=0 and game.players[game.active_player].hand[index].color!=game.table.cards[0].color:
-       if [card for card in game.players[game.active_player].hand if card.color==game.table.cards[0].color]:
-        return render_template('solo/throw_card.html', game=game)
+    if game.table.count_cards() != 0 and game.players[game.active_player].hand[index].color != game.table.cards[0].color:
+        if [card for card in game.players[game.active_player].hand if card.color == game.table.cards[0].color]:
+            return render_template('solo/throw_card.html', game=game)
     game.play(index)
     return redirect('/solo/next_player', code=302)
 
+
 @app.route('/solo/table')
 def solo_table():
-    if game.table.count_cards()<4:
+    if game.table.count_cards() < 4:
         return redirect('/solo/phase1', code=302)
     else:
         game.count_played_cards()
         strongest_card = game.table.cards[0]
         player_index = -1
         for card in game.table.cards:
-            if card.value > strongest_card.value and card.color==game.table.cards[0].color:
-                strongest_card=card
+            if card.value > strongest_card.value and card.color == game.table.cards[0].color:
+                strongest_card = card
         for i in range(4):
             if strongest_card in game.players[i].thrown_cards:
                 game.players[i].add_to_taken(game.table.cards)
@@ -81,15 +88,16 @@ def solo_table():
         game.active_player = player_index
         return render_template('solo/table.html', game=game, player_index=player_index)
 
+
 @app.route('/solo/end_of_round')
 def solo_end_of_round():
-    if game.round ==0:
+    if game.round == 0:
         game.new_round()
         game.deal_cards()
         for player in game.players:
             player.sort_hand()
         return redirect('/solo/phase1', code=302)
-    game.rounds_points[game.round](game.round-1)
+    game.rounds_points[game.round](game.round - 1)
     for player in game.players:
         player.hand = []
     if game.round < 7:
@@ -103,6 +111,7 @@ def solo_end_of_round():
     else:
         return redirect('/solo/end_of_game', code=302)
 
+
 @app.route('/solo/end_of_game')
 def solo_end_of_game():
     winners = []
@@ -115,48 +124,57 @@ def solo_end_of_game():
             winners.append(player)
     return render_template('solo/end_of_game.html', game=game, winners=winners)
 
+
 @app.route('/solo/next_player')
 def solo_next_player():
     game.active_player += 1
     game.active_player %= 4
     return redirect('/solo/table', code=302)
 
+
 # HOTSEAT ##################################################################################
 @app.route('/hotseat/form', methods=['GET', 'POST'])
 def hotseat_form():
     if request.method == 'POST':
         players = [x for x in request.form.getlist('names') if x]
-        if len(players)==4:
+        if len(players) == 4:
             return hotseat_new_game(players)
     return render_template('hotseat/form.html')
+
 
 def hotseat_new_game(name):
     global game
     game = Game(name, 'hotseat')
     return redirect('/hotseat/phase1', code=302)
 
+
 @app.route('/hotseat/phase1')
 def hotseat_phase1():
-    if game.round ==0:
-        game.new_round()
-        game.deal_cards()
-        for player in game.players:
-            player.sort_hand()
-    if len([player for player in game.players if player.count_player_cards() > 0])==0 and 7 >= game.round > 0:
-        return redirect('/hotseat/end_of_round', code=302)
-    game.table.cards = []
+    if len([player for player in game.players if player.count_player_cards() > 0]) == 0 and game.round <= 7:
+        return redirect('/solo/end_of_round', code=302)
+    if game.table.count_cards() == 4:
+        game.table.cards = []
     return render_template('hotseat/throw_card.html', game=game)
 
+@app.route('/hotseat/throw_card/<int:index>')
+def hotseat_throw_card(index):
+    if game.table.count_cards() != 0 and game.players[game.active_player].hand[index].color != game.table.cards[0].color:
+        if [card for card in game.players[game.active_player].hand if card.color == game.table.cards[0].color]:
+            return render_template('hotseat/throw_card.html', game=game)
+    game.table.put_on_table(game.players[game.active_player].hand[index])
+    game.players[game.active_player].add_to_thrown(game.players[game.active_player].hand[index])
+    game.players[game.active_player].throw(index)
+    return redirect('/hotseat/next_player', code=302)
 @app.route('/hotseat/table')
 def hotseat_table():
-    if game.table.count_cards()<4:
+    if game.table.count_cards() < 4:
         return render_template('hotseat/throw_card.html', game=game)
     if game.table.count_cards() == 4:
         strongest_card = game.table.cards[0]
         player_index = -1
         for card in game.table.cards:
-            if card.value > strongest_card.value and card.color==game.table.cards[0].color:
-                strongest_card=card
+            if card.value > strongest_card.value and card.color == game.table.cards[0].color:
+                strongest_card = card
         for i in range(4):
             if strongest_card in game.players[i].thrown_cards:
                 game.players[i].add_to_taken(game.table.cards)
@@ -165,18 +183,15 @@ def hotseat_table():
         game.active_player = player_index
         return render_template('hotseat/table.html', game=game, player_index=player_index)
 
-@app.route('/hotseat/throw_card/<int:index>')
-def hotseat_throw_card(index):
-    if game.table.count_cards()!=0 and game.players[game.active_player].hand[index].color!=game.table.cards[0].color:
-       if [card for card in game.players[game.active_player].hand if card.color==game.table.cards[0].color]:
-        return render_template('hotseat/throw_card.html', game=game)
-    game.table.put_on_table(game.players[game.active_player].hand[index])
-    game.players[game.active_player].add_to_thrown(game.players[game.active_player].hand[index])
-    game.players[game.active_player].throw(index)
-    return redirect('/hotseat/next_player', code=302)
 
 @app.route('/hotseat/end_of_round')
 def hotseat_end_of_round():
+    if game.round == 0:
+        game.new_round()
+        game.deal_cards()
+        for player in game.players:
+            player.sort_hand()
+        return redirect('/hotseat/phase1', code=302)
     game.rounds_points[game.round](game.round-1)
     for player in game.players:
         player.hand = []
@@ -192,7 +207,7 @@ def hotseat_end_of_round():
         return redirect('/hotseat/end_of_game', code=302)
 
 @app.route('/hotseat/end_of_game')
-def hotsea_end_of_game():
+def hotseat_end_of_game():
     winners = []
     max_points = 0
     for player in game.players:
@@ -203,12 +218,13 @@ def hotsea_end_of_game():
             winners.append(player)
     return render_template('hotseat/end_of_game.html', game=game, winners=winners)
 
+
 @app.route('/hotseat/next_player')
-def hotsea_next_player():
+def hotseat_next_player():
     game.active_player += 1
     game.active_player %= 4
     return redirect('/hotseat/table', code=302)
 
+
 if __name__ == '__main__':
     app.run(host='wierzba.wzks.uj.edu.pl', port=5103, debug=True)
-
